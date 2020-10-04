3a8b018cc252d5a94c4d8a6999675997d6ea11dd
HenryBass <63601449+HenryBass@users.noreply.github.com>
2020-10-04 11:05:20 -0400

    Update RandomHills.py

--- RandomHills.py
+++ RandomHills.py
@@ -1,59 +1,37 @@
-import graphics
+from graphics import *
+import random
+import time
 
 win_size = 400
-x = 0
-xcount = 0
-y = 0
-ycount = 0
-redcount = 0
+win = GraphWin("Hills", win_size, win_size)
 
-win = graphics.GraphWin("Primes", win_size, win_size)
-graph = graphics.GraphWin("Prime Population", win_size, win_size)
-time = 0
+while True:
+    y = 0
+    x = 1
+    b = 0
+    g = 0
+    lasty = 200
+    for i in range(0, win_size):
+        while y != (lasty - 1) and y != (lasty + 1):
+            y = random.randint(0, 400)
+        if y > lasty:
+            r = 255
+        else:
+            r = 0
+        color = color_rgb(r, g, b)
+        point = Point(x, y)
+        point.setFill(color)
 
-print("This is a Map of Primes less than 160000 \n"
-      "and a Live Graph of Prime Population per 400 Integers.")
-while x < win_size * win_size:
-    point = graphics.Point(x, y)
-
-    color = "red"
-    i = 2
-
-    while i < xcount:
-
-        if xcount%i == 0:
-            color = "black"
-            break
-        i = i+1
-
-
-    if color == "red":
-        redcount += 1
-    point.setFill(color)
-
-    x+= 1
-    xcount += 1
-    if y > win_size ^ 2:
-        break
-    try:
-        point.draw(win)
-    except:
-        quit()
-    if x >= win_size:
-        y+= 1
-        ycount += 1
-        x = 0
-        primepop = win_size / redcount
-        time += 1
-        graphdot = graphics.Point(time, primepop)
-        graphdot.setFill("red")
         try:
-            graphdot.draw(graph)
+            point.draw(win)
         except:
-            quit()
-        redcount = 0
-
-try:
-    win.getMouse()
-except:
-    win.close()
+            win.close()
+        x += 1
+        lasty = y
+        time.sleep(.01)
+    shape = Rectangle(Point(0, 0), Point(400, 400))
+    shape.setFill("white")
+    try:
+        shape.draw(win)
+    except:
+        win.close()
